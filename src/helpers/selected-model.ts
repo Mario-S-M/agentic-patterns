7fc171788214105ebd848a514c961ce7fdb01490
Mario Eduardo Sánchez Mejía <mayitolalito@hotmail.com>
2026-09-23 17:55:02 -0600

    Change Model

--- src/helpers/selected-model.ts
+++ src/helpers/selected-model.ts
@@ -6,28 +6,32 @@
 
 // Groq:
 // npm i @ai-sdk/groq
-import { groq } from '@ai-sdk/groq';
+// import { groq } from '@ai-sdk/groq';
 
 // Anthropic:
 // npm i @ai-sdk/anthropic
-import { anthropic } from '@ai-sdk/anthropic';
+// import { anthropic } from '@ai-sdk/anthropic';
 
 // OpenAI:
 // npm i @ai-sdk/openai
-import { openai } from '@ai-sdk/openai';
+// import { openai } from '@ai-sdk/openai';
 
 // Gemini:
 // npm i @ai-sdk/google
-import { google } from '@ai-sdk/google';
+// import { google } from '@ai-sdk/google';
 
 // Ollama:
 // npm i ollama-ai-provider-v2
 // A diferencia de los anteriores, este es un proveedor comunitario.
 import { ollama } from 'ollama-ai-provider-v2';
 
+
 // Groq:
-export const model = groq('openai/gpt-oss-20b');
+// export const model = groq('openai/gpt-oss-20b');
 // export const model = groq('openai/gpt-oss-120b');
+
+
+
 
 // Anthropic:
 // export const model = anthropic('claude-haiku-4-5');
@@ -39,4 +43,9 @@ export const model = groq('openai/gpt-oss-20b');
 // export const model = google('gemini-2.5-flash');
 
 // Ollama:
+export const model = ollama('glm-4.7-flash');
 // export const model = ollama('gpt-oss');
+// export const model = ollama('llama3.3:70b'); // ⚠️ Tener cuidado
+//export const model = ollama('qwen3-coder-next:latest');
+// export const model = ollama('llama3.2');
+// export const model = ollama('gemma4:31b');
